df2446dd454c59d9a09fe6340973653d9c396580
VELARA <velaragirlgroup@gmail.com>
2026-08-13 13:55:01 +0200

    Update release year from 2026 to 2027

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -294,7 +294,7 @@ export default function Home() {
                     className="text-[#c9956c] text-lg"
                     style={{ fontFamily: "'Cormorant Garamond', serif", fontWeight: 600 }}
                   >
-                    Debut EP · 2026
+                    Debut EP · 2027
                   </div>
                 </div>
                 <Play size={20} className="text-[#c9956c]/50" />
